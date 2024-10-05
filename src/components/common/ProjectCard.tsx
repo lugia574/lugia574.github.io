89ff9b7d032e2f76a115d296d020994036a06f9e
lugia574 <66871221+lugia574@users.noreply.github.com>
2024-10-05 20:51:46 +0900

    project img tag 와 overlay 뼈대 구현

--- src/components/common/ProjectCard.tsx
+++ src/components/common/ProjectCard.tsx
@@ -11,9 +11,12 @@ const ProjectCard = ({ name }: Props) => {
     <ProjectCardStyle>
       <div className="flip-box" onClick={() => setIsFlipped((prev) => !prev)}>
         <div className={`flip ${isFlipped ? "flipped" : ""}`}>
-          <div className="front">앞면 이미지</div>
+          <div className="front">
+            <img src="/images/7days-main.png" alt="먼데" />
+            <div className="overlay">클릭하세요</div>
+          </div>
           <div className="back">
-            <h4 className="Project-name">{name}</h4>
+            <div className="project-name">{name}</div>
           </div>
         </div>
       </div>
@@ -25,7 +28,7 @@ const ProjectCardStyle = styled.div`
   flex-wrap: nowrap;
   .flip-box {
     display: flex;
-    width: 300px;
+    width: 400px;
     height: 400px;
     perspective: 1000px;
     cursor: pointer;
@@ -43,30 +46,59 @@ const ProjectCardStyle = styled.div`
     transition: 0.5s;
   }
 
-  .front {
-    background-color: red;
+  .front img {
+    width: 400px;
+    height: 400px;
+    object-fit: cover;
+    object-position: top;
   }
+
+  .overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5); /* 반투명 배경 */
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    opacity: 0;
+    transition: opacity 0.3s ease;
+    z-index: 1;
+  }
+
+  .flip:hover .overlay {
+    opacity: 1;
+  }
+
   .back {
     background-color: green;
     transform: rotateY(180deg);
   }
 
   .front,
+  .front-hover,
   .back {
     width: 100%;
     height: 100%;
     position: absolute;
     backface-visibility: hidden;
     border-radius: 5px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 
   .flipped {
     transform: rotateY(-180deg); /* 상태가 flipped일 때 뒷면 보이도록 */
   }
 
-  /* .flip-box:hover .flip {
-    transform: rotateY(-180deg);
-  } */
+  .project-name {
+    width: auto;
+    height: auto;
+  }
 `;
 
 export default ProjectCard;
